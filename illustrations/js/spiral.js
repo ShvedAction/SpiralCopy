04fe75adfa1aea6f7393c30ca5273a31838227ed
Aleksey <shvedujk@gmail.com>
2019-03-18 21:38:27 +0300

    calculate circle index from first sum of count element of circle

--- illustrations/js/spiral.js
+++ illustrations/js/spiral.js
@@ -2,9 +2,13 @@
     "use strict"
     const tls = tools()
 
-    const [N, M] = [10, 12]
+    const [N, M] = [8, 8]
 
     const field = document.getElementById('field')
     const matrix = tls.buildField(N, M, field)
-    tls.fillMatrixRandoms(matrix)
+    //tls.fillMatrixRandoms(matrix)
+
+    tls.speralItterator(N, M, (x, y, index) =>{
+        matrix[y][x].innerHTML = '' + tls.spiralCircleIndex(index, N, M)
+    })
 })()
--- illustrations/js/tools.js
+++ illustrations/js/tools.js
@@ -1,5 +1,5 @@
 function tools(){
-    return {
+    const tools = {
         /*
             Build field from divs representing a two dimension matrix.
             n is count of columns
@@ -30,6 +30,64 @@ function tools(){
                     row[columnIndex].innerHTML = Math.round(Math.random() * 9)
                 }
             }
+        },
+
+        /*
+            Itterate spiral
+            def callback(x, y, index)
+        */
+        speralItterator: (n, m, callback, startPoint = [0, 0], direct=[1,0], offset=0) => {
+            if (n*m < 1)
+                return false
+
+            const [xdirect, ydirect] = direct
+            let [x, y] = startPoint
+
+            const countEdgeStep = Math.abs(xdirect) * n + Math.abs(ydirect) * m
+            for(let index = offset; index < countEdgeStep + offset - 1; index++){
+                callback(x, y, index)
+                x += xdirect
+                y += ydirect
+            }
+
+
+            const rotateDirect = [-ydirect, xdirect]
+
+            callback(x, y, countEdgeStep + offset - 1)
+
+            x += rotateDirect[0]
+            y += rotateDirect[1]
+
+            tools.speralItterator(n - Math.abs(rotateDirect[0]), m - Math.abs(rotateDirect[1]), callback, [x, y], rotateDirect, countEdgeStep + offset)
+        },
+
+        /*
+            calculate circle index
+        */
+        spiralCircleIndex: (index, n, m) => {
+            //index from center
+            const i = n * m - 1 - index 
+
+            const minDim = Math.min(n, m)
+            let a0, a1
+            if (m == n){
+                a0 = minDim % 2
+                a1 = 4 + (minDim  % 2) * 4
+            }else{
+                a0 = (minDim % 2) * (Math.abs(m - n) + 1)
+                a1 = (minDim % 2 + 1) *(Math.abs(m - n) + 1) + 6 * (minDim % 2)
+            }
+
+            return Math.floor((Math.sqrt(Math.pow(4 - a1, 2) - 16*(a0 - i)) + 4 - a1) / 8) + 1
+        },
+
+        /*
+            calculate cordinates by index for spiral copy problem
+        */
+        spiralCordinates: (index, n, m) => {
+            //todo:
         }
     }
+
+    return tools
 }
