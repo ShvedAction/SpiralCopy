d7942ab1a9507b21fd24a991d108f5c8601302ba
Aleksey <shvedujk@gmail.com>
2019-03-21 15:04:13 +0300

    The illustration buildig an one-dimensional result array

--- illustrations/js/spiral.js
+++ illustrations/js/spiral.js
@@ -2,26 +2,36 @@
     "use strict"
     const tls = tools()
 
-    const [N, M] = [9, 9]
+    const [N, M] = [5, 5]
 
     const field = document.getElementById('field')
+    const linerOut = document.getElementById('line')
     const matrix = tls.buildField(N, M, field)
     tls.fillMatrixRandoms(matrix)
     const flashOneByOne = async function(){
+        let delay = 150
+        const acceleration = 2
         for(let i = 0; i < N*M; i++){
-            await timeout(120)
+            await timeout(delay)
+            delay -= acceleration
             const [xc, yc] = tls.spiralCordinates(i, N, M)
             matrix[yc][xc].classList = "flashMe"
+            const cell = document.createElement('div')
+            cell.classList = "flashMe"
+            cell.innerHTML = matrix[yc][xc].innerHTML
+            linerOut.appendChild(cell)
        }
     }
 
     flashOneByOne()
 
+    const [xc, yc] = tls.spiralCordinates(0, N, M)
+
     // tls.speralItterator(N, M, (x, y, index) =>{
     //     const [xc, yc] = tls.spiralCordinates(index, N, M)
     //     const circleIndex = tls.spiralCircleIndex(index, N, M)
-    //     //matrix[y][x].innerHTML = '' + xc + ';'+  yc
-    //     matrix[y][x].innerHTML = ''   + index //+ ' ;' +  circleIndex
+    //     matrix[y][x].innerHTML = '' + xc + ';'+  yc+ ' ;' +  circleIndex
+    //     //matrix[y][x].innerHTML = ''   + index + ' ;' +  circleIndex
     //     //matrix[y][x].innerHTML = '' + x + ';'+  y
     // })
 
